6f3ed45011a9e6f25c348b05a90eb0ba9b78e6c5
Victorien Elvinger <victorien@elvinger.fr>
2024-02-12 17:49:29 +0100

    test: add regression tests

---
+++ crates/biome_js_analyze/tests/specs/nursery/noInvalidUseBeforeDeclaration/invalidBindingPattern.js
@@ -0,0 +1,14 @@
+function f({ b = a, a = 0 }) {}
+function f([b = a, a = 0]) {}
+function f({ x: [b = a, { a = 0 }] }) {}
+{
+	const { b = a, a = 0 } = {};
+}
+{
+	const [b = a, a = 0] = {};
+}
+{
+	const {
+		x: [b = a, { a = 0 }],
+	} = {};
+}
